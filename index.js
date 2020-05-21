290df954633adeca9905dd4eb4bca9285b6d1507
PawFV <44122984+PawFV@users.noreply.github.com>
2020-05-21 19:43:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 var cors = require('cors');
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Origin", "https://paw-webdev.web.app"); 
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
